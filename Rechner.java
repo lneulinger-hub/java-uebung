9586e3d26d8a13b12112a3c3b6f2de66e29041a8
lneulinger-hub <lneulinger@tsn.at>
2026-06-09 15:25:46 +0200

    comit1

---
+++ Rechner.java
@@ -0,0 +1,8 @@
+int[] werte = {10, 20};
+        int summe = 0;
+        for (int i = 0; i < werte.length; i++) {
+            summe += werte[i];
+        }
+        System.out.println("Die Summe der Werte ist: " + summe);
+    }
+}
