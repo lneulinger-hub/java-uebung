8e409d32237d3143637fb2431a819107b35ced33
lneulinger-hub <lneulinger@tsn.at>
2026-06-09 15:21:48 +0200

    Ausgabe Name & Datum

--- HalloWelt.java
+++ HalloWelt.java
@@ -1,5 +1,7 @@
 public class HalloWelt {
     public static void main(String[] args) {
         System.out.println("Hallo GitHub!"); // Diese Zeile habe ich auf GitHub bearbeitet
+        System.out.println("Lena");
+        System.out.println("09.06.2026");
     }
 }
