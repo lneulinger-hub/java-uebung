22e4f2aede9421450990ea0676af7144ed47d029
lneulinger-hub <lneulinger@tsn.at>
2026-06-16 15:22:41 +0200

    neu

--- programm_neu/AlterCheck.java
+++ programm_neu/AlterCheck.java
@@ -1,5 +1,21 @@
 package programm_neu;
 
-public class AlterCheck {
+import javax.swing.JOptionPane; 
+
+  
+public class AlterCheck { 
+
+    public static void main(String[] args) { 
+
+            String input = JOptionPane.showInputDialog("Bitte geben Sie Ihr Alter ein:");
+            int alter = Integer.parseInt(input);
+
+            if (alter >= 18) {
+                System.out.println("volljährig.");
+            } else {
+                System.out.println("minderjährig.");
+
+            }
     
+     }
 }
